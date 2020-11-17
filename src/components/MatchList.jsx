c6019111a7284862bd973e393639c50835548d39
TSKraak <tskraak@gmail.com>
2020-11-17 12:27:46 +0100

    Displaying ongoing & finished matches with players & score

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Box } from "@material-ui/core";
 import { useQuery } from "@apollo/react-hooks";
 import ErrorIcon from "@material-ui/icons/Error";
-
+import moment from "moment";
 import { GET_ALL_MATCHES } from "../graphql/queries";
 
 const useStyles = makeStyles((theme) => ({
@@ -16,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
 function MatchList() {
   const classes = useStyles();
   const { loading, error, data } = useQuery(GET_ALL_MATCHES);
+  console.log("DATA:", data);
 
   if (loading) return "Loading...";
   if (error)
@@ -26,14 +27,74 @@ function MatchList() {
       </p>
     );
 
+  const finishedMatches = data.matches.filter((match) => match.finished);
+  const ongoingMatches = data.matches.filter((match) => !match.finished);
+  // console.log("finished:", finishedMatches);
+  // console.log("ongoing:", ongoingMatches);
+
   return (
     <Container className={classes.root}>
-      <Typography variant="h2">All MAtches</Typography>
+      <Typography variant="h2">Ongoing Matches</Typography>
       <Box>
-        {data.matches.map((match) => (
+        {ongoingMatches.map((match) => (
+          <article key={match.id}>
+            <p>Match ID: {match.id}</p>
+            <p>
+              Match date:{" "}
+              {moment(match.started_at).format("DD-MM-YYYY hh:mm:ss")}
+            </p>
+            <p>
+              {match.p1.name} vs {match.p2.name}
+            </p>
+
+            <ul>
+              <b>Sets:</b>
+
+              {match.setts.map((set, i) => (
+                <li key={i}>
+                  {set.p1_score} - {set.p2_score}
+                </li>
+              ))}
+            </ul>
+
+            {match.winner ? (
+              <p>
+                <b>Winner: {match.winner.name}</b>
+              </p>
+            ) : null}
+            <p>Status: {match.finished ? "Match finished" : "Ongoing"}</p>
+            <hr />
+          </article>
+        ))}
+      </Box>
+      <br></br>
+      <br></br>
+      <Typography variant="h2">Finished Matches</Typography>
+      <Box>
+        {finishedMatches.map((match) => (
           <article key={match.id}>
             <p>Match ID: {match.id}</p>
             <p>Match date: {match.started_at}</p>
+            <p>
+              {match.p1.name} vs {match.p2.name}
+            </p>
+
+            <ul>
+              <b>Sets:</b>
+
+              {match.setts.map((set, i) => (
+                <li key={i}>
+                  {set.p1_score} - {set.p2_score}
+                </li>
+              ))}
+            </ul>
+
+            {match.winner ? (
+              <p>
+                <b>Winner: {match.winner.name}</b>
+              </p>
+            ) : null}
+            <p>Status: {match.finished ? "Match finished" : "Ongoing"}</p>
             <hr />
           </article>
         ))}
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,10 +1,27 @@
 import gql from "graphql-tag";
 
 export const GET_ALL_MATCHES = gql`
-  query AllMatches {
-    matches {
+  query LiveMatches {
+    matches(order_by: { finished: asc, started_at: asc }) {
       id
       started_at
+      p1 {
+        id
+        name
+      }
+      p2 {
+        id
+        name
+      }
+      setts {
+        p1_score
+        p2_score
+      }
+      winner {
+        id
+        name
+      }
+      finished
     }
   }
 `;
